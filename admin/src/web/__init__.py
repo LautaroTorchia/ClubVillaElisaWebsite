f4ff7c1f54a05e8a4432995929aed4759e796d75
Juan Tomás Vicens <juantvicens@gmail.com>
2022-09-18 21:13:16 -0300

    first config update - doesn´t work

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from src.web.config import config
 
-
-def create_app():
+def create_app(env="development"):
     app = Flask(__name__)
+    app.config.from_object(config[env])
 
     @app.get("/")
     def home():
-        return "Hello, world!"
+        return "Hello, world!" + 12
 
     return app
