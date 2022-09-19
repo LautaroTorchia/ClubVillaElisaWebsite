27239ec2533b47a6beb046711acd2e6e1529c6e3
Juan Tomás Vicens <juantvicens@gmail.com>
2022-09-18 21:13:16 -0300

    first config update - doesn´t work

--- admin/src/web/config.py
+++ admin/src/web/config.py
@@ -1,30 +1,32 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-FLASK_ENV = 'development'
-SECRET_KEY = 'GDtfDCFYjD'
+from dotenv import load_dotenv
+load_dotenv()
+
 
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = "postgresql:///aca va el nombre de la bd"
 
 
 class ProductionConfig(Config):
     DEBUG = False
 
-
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+    FLASK_ENV = "development"
 
 
 class TestingConfig(Config):
     TESTING = True
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+}
