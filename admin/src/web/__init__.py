6ec99cac5a9455bb82e00104fa619a4d0e386f54
Ignacio Traberg <ignacio.traberg@gmail.com>
2022-09-23 16:21:43 -0300

    Private landing page placeholder

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 from src.web.config import config
 from src.core.db import db, init_db
 
-def create_app(env="development"):
+def create_app(env="development", static_folder="static", template_folder="templates"):
     app = Flask(__name__)
     app.config.from_object(config[env])
     with app.app_context():
@@ -10,9 +10,18 @@ def create_app(env="development"):
 
     @app.get("/")
     def home():
-        return "Hello, world!"
+        return render_template('index.html')
+    #@app.get("/listado_socios")
+    #def listado_socios():
+    #    return render_template('listado_socios.html')
+    #@app.get("/configuracion")
+    #def config():
+    #    return render_template('configuracion.html')
+    #@app.get("/public_index")
+    #def public_home():
+    #    return render_template('public_index.html')
     @app.teardown_appcontext
     def shutdown_session(exception=None):
         db.session.remove()
-    
+
     return app
