17d3441a7d72b9e1ddea41fa72bf6336bf6982ad
Ignacio Traberg <ignacio.traberg@gmail.com>
2022-09-25 13:42:39 -0300

    'config_db' and 'reset_db' methods added to db.py. 'db' var changes to 'database'

--- admin/src/core/db.py
+++ admin/src/core/db.py
@@ -1,18 +1,34 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import current_app as app
-from flask_migrate import Migrate
+#from flask_migrate import Migrate
 
-db = SQLAlchemy()
-from src.core.board.user import User
+database = SQLAlchemy()
+from src.core.auth.user import User
 from src.core.board.discipline import Discipline
 from src.core.board.associate import Associate
-from src.core.board.role import Role
+from src.core.auth.role import Role
 from src.core.board.payment import Payment
-from src.core.board.permission import Permission
-migrate = Migrate()
+from src.core.auth.permission import Permission
+#migrate = Migrate()
 
-def init_db(app):
-    db.init_app(app)
-    migrate.init_app(app, db)
-    #db.create_all()
+def init_app(app):
+    database.init_app(app)
+    config_db(app)
+    #migrate.init_app(app, db)
+
+def config_db(app):
+    @app.before_first_request
+    def create_tables():
+        database.create_all()
+    
+    @app.teardown_appcontext
+    def close_session(exception=None):
+        database.session.remove()
+
+def reset_db():
+    print("   Deleting database...")
+    database.drop_all()
+    print("   Creating database ...")
+    database.create_all()
+    print("   All done!")
 
--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,18 +1,22 @@
 from flask import Flask
 from src.web.config import config
-from src.core.db import db, init_db
+from src.core.db import database, init_app
 
 def create_app(env="development"):
     app = Flask(__name__)
     app.config.from_object(config[env])
     with app.app_context():
-        init_db(app)
+        init_app(app)
 
     @app.get("/")
     def home():
         return "Hello, world!"
     @app.teardown_appcontext
     def shutdown_session(exception=None):
-        db.session.remove()
+        database.session.remove()
     
+    @app.cli.command("reset-db")
+    def reset_db():
+        database.reset_db()
+
     return app
