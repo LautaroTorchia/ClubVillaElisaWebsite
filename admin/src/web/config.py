c77a026665ea2484ccaa3ae6c746babdb3210a33
Ignacio Traberg <ignacio.traberg@gmail.com>
2022-09-25 13:51:18 -0300

    connection to db info configured into config.py

--- admin/src/web/config.py
+++ admin/src/web/config.py
@@ -1,4 +1,5 @@
 import os
+from os import environ as env
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 from dotenv import load_dotenv
@@ -14,11 +15,26 @@ class Config(object):
 
 class ProductionConfig(Config):
     DEBUG = False
+    DB_USER = env.get("DB_USER")
+    DB_PASSWORD = env.get("DB_PASSWORD")
+    DB_HOST = env.get("DB_HOST")
+    DB_NAME = env.get("DB_NAME")
+    DB_PORT = env.get("DB_PORT")
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 class DevelopmentConfig(Config):
     ENV = "development"
     DEVELOPMENT = True
     DEBUG = True
+    DB_USER = env.get("DB_USER", "postgres")
+    DB_PASSWORD = env.get("DB_PASSWORD", "postgres")
+    DB_HOST = env.get("DB_HOST", "localhost")
+    DB_NAME =  env.get("DB_NAME", "grupo12")
+    DB_PORT = env.get("DB_PORT", "5432")
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
 
 class TestingConfig(Config):
     TESTING = True
