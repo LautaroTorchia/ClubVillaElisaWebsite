b861b2ff1dd9f38cdbcf10dec420af56c79f2a29
Juan Tomás Vicens <juantvicens@gmail.com>
2022-09-25 15:29:57 -0300

    Change import in db.py

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,23 +1,23 @@
 from flask import Flask
 from src.web.config import config
-from src.core import db
+import src.core.db as database
 
 def create_app(env="development"):
     app = Flask(__name__)
     app.config.from_object(config[env])
     with app.app_context():
-        db.init_app(app)
+        database.init_app(app)
 
     @app.get("/")
     def home():
         return "Hello, world!"
     @app.teardown_appcontext
     def shutdown_session(exception=None):
-        db.session.remove()
+        database.db.session.remove()
     
     @app.cli.command("resetdb")
     def resetdb():
-        db.reset_db()
+        database.reset_db()
 
 
     return app
