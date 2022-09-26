c6587a3913723d2b4f00db421ccd5a050a1a3f26
Juan Tomás Vicens <juantvicens@gmail.com>
2022-09-25 21:17:06 -0300

    added disciplines blueprint

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,16 +1,21 @@
 from flask import Flask
 from src.web.config import config
 import src.core.db as database
+from src.web.controllers.discipline import discipline_blueprint
 
 def create_app(env="development"):
     app = Flask(__name__)
     app.config.from_object(config[env])
+    app.register_blueprint(discipline_blueprint)
+
     with app.app_context():
         database.init_app(app)
 
     @app.get("/")
     def home():
         return "Hello, world!"
+
+
     @app.teardown_appcontext
     def shutdown_session(exception=None):
         database.db.session.remove()
