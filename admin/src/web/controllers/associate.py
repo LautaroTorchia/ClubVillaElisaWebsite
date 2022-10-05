fd6b6db04989deed97a704ca9743dedb7a7652b4
Lautaro Torchia <torchia.lautaro2@gmail.com>
2022-10-05 09:01:54 -0300

    enable/disable associate implemented, still need to discuss its effect

--- admin/src/web/controllers/associate.py
+++ admin/src/web/controllers/associate.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for,flash
-from src.core.board import create_associate, delete_associate, delete_discipline, get_associate_by_id, list_associates, update_associate
+from src.core.board import create_associate, delete_associate, delete_discipline, disable_associate, enable_associate, get_associate_by_id, list_associates, update_associate
 from src.core.board.associate import Associate
 from src.web.forms.associate import CreateAssociateForm, UpdateAssociateForm
 from src.web.helpers.form_utils import csrf_remover
@@ -49,4 +49,17 @@ def post_update(id):
         update_associate(form,id)
         return redirect(url_for("associate.index"))
     return render_template("associate/add.html", form=form)
-        
+
+#disabling associates
+@associate_blueprint.post("/disable/<id>")
+def disable(id):
+    flash(f"Se deshabilito al asociado satisfactoriamente", category="alert alert-warning")
+    disable_associate(id)
+    return redirect(url_for("associate.index"))
+
+#disabling associates
+@associate_blueprint.post("/enable/<id>")
+def enable(id):
+    flash(f"Se habilito al asociado satisfactoriamente", category="alert alert-warning")
+    enable_associate(id)
+    return redirect(url_for("associate.index"))
