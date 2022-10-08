729879d8f3453e07be88dae4518efa796973d88a
Juan Tomás Vicens <juantvicens@gmail.com>
2022-10-08 00:00:16 -0300

    Fixed blueprint url

--- admin/src/web/controllers/api/discipline.py
+++ admin/src/web/controllers/api/discipline.py
@@ -4,7 +4,7 @@ from src.core.board import list_disciplines
 
 
 discipline_api_blueprint = Blueprint(
-    "discipline_api", __name__, url_prefix="/api/discipline"
+    "discipline_api", __name__, url_prefix="/api/club/disciplines"
 )
 
 @discipline_api_blueprint.get("/")
