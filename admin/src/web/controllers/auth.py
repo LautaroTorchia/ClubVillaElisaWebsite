4bb5af63c89b1e2de290c710dfe8e9a14e6a4c79
fots <facundotomatis@gmail.com>
2022-10-08 03:15:09 -0300

    logout and login implementation

--- admin/src/web/controllers/auth.py
+++ admin/src/web/controllers/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect,render_template,request,url_for,flash
+from flask import Blueprint, redirect,render_template,request,url_for,flash,session
 from src.core.auth import get_by_usr_and_pwd
 
 auth_blueprint=Blueprint("auth",__name__,url_prefix="/autenticar")
@@ -15,10 +15,12 @@ def authenticate():
     if not user:
         flash("Usuario o clave incorrecta.", category="alert alert-warning")
         return redirect(url_for('auth.login'))
-    #session["user"]=user.username
+    session["user"]=user.username
     flash("La sesion se inicio correctamente", category="info info-warning")
     return redirect(url_for('home'))
 
 @auth_blueprint.get("/logout")
 def logout():
-    pass
+    del session["user"]
+    session.clear()
+    return redirect(url_for('auth.login'))
