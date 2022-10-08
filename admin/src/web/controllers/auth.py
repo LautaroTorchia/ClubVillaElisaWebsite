1e1d8122052ca0be151c2d96e4eb95789b5a29f5
Juan Tomás Vicens <juantvicens@gmail.com>
2022-10-08 20:00:49 -0300

    Added form from wtforms to login

--- admin/src/web/controllers/auth.py
+++ admin/src/web/controllers/auth.py
@@ -1,23 +1,26 @@
 from flask import Blueprint, redirect,render_template,request,url_for,flash,session
 from src.core.auth import get_by_usr_and_pwd
 from src.web.helpers.auth import login_required
+from src.web.forms.login import LoginForm
 
 auth_blueprint=Blueprint("auth",__name__,url_prefix="/autenticar")
 
 @auth_blueprint.get("/")
 def login():
-    return render_template("login.html")
+    return render_template("login.html",form=LoginForm())
 
 @auth_blueprint.post("/authenticate")
 def authenticate():
-    usr=request.form["username"]
-    pwd=request.form["password"]
-    user=get_by_usr_and_pwd(usr,pwd)
-    if not user:
-        flash("Usuario o clave incorrecta.", category="alert alert-warning")
-        return redirect(url_for('auth.login'))
-    session["user"]=user.username
-    flash("La sesion se inicio correctamente", category="info info-warning")
+    form=LoginForm(request.form)
+    if form.validate():
+        usr=request.form["username"]
+        pwd=request.form["password"]
+        user=get_by_usr_and_pwd(usr,pwd)
+        if not user:
+            flash("Usuario o clave incorrecta.", category="alert alert-danger w-50")
+            return redirect(url_for('auth.login'))
+        session["user"]=user.username
+        flash("La sesion se inicio correctamente", category="alert alert-success w-50")
     return redirect(url_for('home'))
 
 @auth_blueprint.get("/logout")
