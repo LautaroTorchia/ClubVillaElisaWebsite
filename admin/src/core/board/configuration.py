f3836a656130904eed2cfa554fa7f9ce1bf0f038
fots <facundotomatis@gmail.com>
2022-10-09 23:03:07 -0300

    changed config repr and docstring

--- admin/src/core/board/configuration.py
+++ admin/src/core/board/configuration.py
@@ -8,7 +8,7 @@ class Configuration(db.Model):
         record_number (int): How many records to show
         currency (str): Currency type
         base_fee (float): Base fee used to calculate club fee
-        due_fee (float): Fee due to payment delayed
+        due_fee (int): Fee due % to payment delayed
         payment_available (bool): Toggle payment table in public app
         contact (str): Contact info
         payment_header (str): Information shown at payment receipt
@@ -27,14 +27,16 @@ class Configuration(db.Model):
     def __init__(self, configuration_data):
         self.record_number = configuration_data["record_number"]
         self.currency = configuration_data["currency"]
-        self.base_fee=configuration_data["base_fee"]
-        self.due_fee=configuration_data["due_fee"]
-        self.payment_available=configuration_data["payment_available"]
-        self.contact=configuration_data["contact"]
-        self.payment_header=configuration_data["payment_header"]
+        self.base_fee = configuration_data["base_fee"]
+        self.due_fee = configuration_data["due_fee"]
+        self.payment_available = configuration_data["payment_available"]
+        self.contact = configuration_data["contact"]
+        self.payment_header = configuration_data["payment_header"]
 
     def __repr__(self):
-        return f"""{self.record_number} elementos en orden {self.ord_criteria}, Moneda: {self.currency}"""
+        return (
+            f"""{self.record_number} cantidad de registros, moneda: {self.currency}"""
+        )
 
     def as_dict(self):
-       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
