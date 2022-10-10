5fbcd41f16387a6917f0294f89a20119ce4461e6
Juan Tomás Vicens <juantvicens@gmail.com>
2022-10-10 17:20:52 -0300

    improved disciplines list api

--- admin/src/web/controllers/api/discipline.py
+++ admin/src/web/controllers/api/discipline.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify
 from src.web.helpers.build_response import response
 from src.core.board import list_disciplines
-
+from datetime import datetime
 
 discipline_api_blueprint = Blueprint(
     "discipline_api", __name__, url_prefix="/api/club/disciplines"
@@ -9,5 +9,4 @@ discipline_api_blueprint = Blueprint(
 
 @discipline_api_blueprint.get("/")
 def index_api():
-    return jsonify(list(map(lambda x: x.to_dict(), list_disciplines())))
-    
+    return response(200,list(map(lambda x: x.to_dict(), list_disciplines())))
