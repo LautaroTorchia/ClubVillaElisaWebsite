8a48ae32979f358a6ff36665f61961b55280f478
Ignacio Traberg <ignacio.traberg@gmail.com>
2022-10-09 21:51:48 -0300

    User model inherits from BaseForm now

--- admin/src/web/forms/base_form.py
+++ admin/src/web/forms/base_form.py
@@ -1,10 +1,6 @@
 from wtforms.validators import Regexp
 from flask_wtf import FlaskForm
 
-
-
-
-
 class BaseForm(FlaskForm):
     alphabetic_validator = Regexp(regex='^(?:[^\W\d_]|[ ])+$', message='El nombre solo puede contener letras y espacios')
     class Meta:
