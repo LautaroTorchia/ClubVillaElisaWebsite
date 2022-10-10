cf314bc297bd439f5585473dbe4630f05c69a3b8
Juan Tomás Vicens <juantvicens@gmail.com>
2022-10-09 21:24:21 -0300

    Added Emailfield to user mail field

--- admin/src/web/forms/user.py
+++ admin/src/web/forms/user.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms.validators import Length, InputRequired
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, EmailField
 
 class UserForm(FlaskForm):
     first_name = StringField('Nombre', validators=[Length(max=255),InputRequired()])
     last_name = StringField('Apellido', validators=[Length(max=255),InputRequired()])
-    email = StringField('Email', validators=[Length(max=255),InputRequired()])
+    email = EmailField('Email', validators=[Length(max=255),InputRequired()])
     username = StringField('Nombre de usuario', validators=[Length(max=255),InputRequired()])
     password = PasswordField('Contraseña', validators=[Length(max=255),InputRequired()])
     
 class UpdateUserForm(FlaskForm):
     first_name = StringField('Nombre', validators=[Length(max=255),InputRequired()])
     last_name = StringField('Apellido', validators=[Length(max=255),InputRequired()])
-    email = StringField('Email', validators=[Length(max=255),InputRequired()])
+    email = EmailField('Email', validators=[Length(max=255),InputRequired()])
     username = StringField('Nombre de usuario', validators=[Length(max=255),InputRequired()])
