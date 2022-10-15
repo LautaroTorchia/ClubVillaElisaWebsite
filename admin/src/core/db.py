6447f719d2240802f0d080036ede7f35dd9169a2
Ignacio Traberg <ignacio.traberg@gmail.com>
2022-10-15 19:36:11 -0300

    db.py docstrings

--- admin/src/core/db.py
+++ admin/src/core/db.py
@@ -11,19 +11,32 @@ from src.core.board.configuration import Configuration
 from src.core.auth.permission import Permission
 
 def init_app(app):
+    """Args:
+        app (Flask): The Flask app.
+    """
     db.init_app(app)
     config_db(app)
 
 def config_db(app):
+    """Args:
+        app (Flask): The Flask app.
+    """    
     @app.before_first_request
     def create_tables():
+        """Create tables in the database.
+        """        
         db.create_all()
     
     @app.teardown_appcontext
     def close_session(exception=None):
+        """Args:
+            exception (Exception, optional): The exception that was raised. Defaults to None.
+        """        
         db.session.remove()
 
 def reset_db():
+    """Reset the database.
+    """    
     print("   Deleting database...")
     db.drop_all()
     print("   Creating database ...")
