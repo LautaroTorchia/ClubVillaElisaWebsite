726ac4004337fdd0987813bc1dd01c81523b6d50
Ignacio Traberg <ignacio.traberg@gmail.com>
2022-10-15 20:58:36 -0300

    Config form docstrings

--- admin/src/web/forms/base_form.py
+++ admin/src/web/forms/base_form.py
@@ -2,8 +2,7 @@ from wtforms.validators import Regexp
 from flask_wtf import FlaskForm
 
 class BaseForm(FlaskForm):
-    """Args:
-        FlaskForm (Form): Form to create a new associate
+    """FlaskForm (Form): Form to create a new associate
     """    
 
     alphabetic_validator = Regexp(regex='^(?:[^\W\d_]|[ ])+$', message='El nombre solo puede contener letras y espacios')
