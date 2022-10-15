858c90b8aed4a284b7e77ea2b3d7279fa6c17014
Ignacio Traberg <ignacio.traberg@gmail.com>
2022-10-15 20:51:15 -0300

    Associate form docstrings

--- admin/src/web/forms/associate.py
+++ admin/src/web/forms/associate.py
@@ -6,6 +6,16 @@ from wtforms.validators import ValidationError
 
 
 class CreateAssociateForm(FlaskForm):
+    """Form to create a new associate
+    name: Name of the associate
+    surname: Surname of the associate
+    email: Email of the associate
+    DNI_number: DNI number of the associate
+    DNI_type: DNI type of the associate
+    gender: Gender of the associate
+    address: Address of the associate
+    phone_number: Phone of the associate
+    """    
     
     name= StringField('Nombre', validators=[validators.DataRequired()])
     surname= StringField('Apellido', validators=[validators.DataRequired()])
@@ -16,16 +26,30 @@ class CreateAssociateForm(FlaskForm):
     address = StringField('Direccion', validators=[validators.input_required()])
     phone_number = StringField('Telefono')
     
-
-    
-    
     def validate_DNI_number(form, field):
+        """Args:
+            form (CreateAssociateForm): Form to create a new associate
+            field (DNI_number): DNI number of the associate
+        Raises:
+            ValidationError: If the DNI number is already in use
+        Returns:
+            Boolean: True if the DNI number is not in use
+        """        
         if get_associate_by_DNI(field.data):
             raise ValidationError('DNI ya registrado')
         return True
 
 
 class UpdateAssociateForm(FlaskForm):
+    """Form to update an associate
+    name: Name of the associate
+    surname: Surname of the associate
+    email: Email of the associate
+    gender: Gender of the associate
+    address: Address of the associate
+    phone_number: Phone of the associate
+    active: Active status of the associate
+    """  
     
     name= StringField('Nombre', validators=[validators.DataRequired()])
     surname= StringField('Apellido', validators=[validators.DataRequired()])
