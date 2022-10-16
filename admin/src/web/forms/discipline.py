5f0781f4bfb3270cc2f5591fe5652bcac8fbc398
Ignacio Traberg <ignacio.traberg@gmail.com>
2022-10-15 21:00:24 -0300

    Discipline form docstrings

--- admin/src/web/forms/discipline.py
+++ admin/src/web/forms/discipline.py
@@ -5,6 +5,14 @@ from src.web.forms.base_form import BaseForm
 instructor_validator = Regexp(regex='^(?:[^\W\d_]|[ ,])+$', message='El nombre de un instructor solo puede contener letras y numeros, por favor separe los nombres de varios instructores con una coma')
 
 class DisciplineForm(BaseForm):
+    """Form to create a new discipline
+    name: Name of the discipline
+    category: Category of the discipline
+    instructors: Name of the instructors
+    dates: Dates and hours of the discipline
+    monthly_cost: Monthly cost of the discipline
+    available: Current availability of the discipline
+    """    
 
     name = StringField('Nombre', validators=[BaseForm.alphabetic_validator,Length(max=255),InputRequired()])
     category = StringField('Categoría',validators=[Length(max=255),InputRequired()])
@@ -14,6 +22,9 @@ class DisciplineForm(BaseForm):
     available = SelectField('Disponible',choices=[(True,'Disponible'),('False','No disponible')],validators=[InputRequired()])
 
     def __init__(self, formdata=..., **kwargs):
+        """Args:
+            formdata (dict): Form data
+        """        
         super().__init__( **kwargs)
         try:
             self['monthly_cost'].label = Label(self['monthly_cost'].id, f"Costo mensual ({kwargs['currency']})")
