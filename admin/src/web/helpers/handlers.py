e59dea42a233242a674e0ef34d17bdd28dbe9900
Ignacio Traberg <ignacio.traberg@gmail.com>
2022-10-15 21:19:36 -0300

    Error handlers helper docstrings

--- admin/src/web/helpers/handlers.py
+++ admin/src/web/helpers/handlers.py
@@ -2,7 +2,11 @@ from flask import render_template
 
 
 def bad_request_error(e):
-    """HTTP Error 400 (Bad Request)"""
+    """Args:
+        e (Exception): Exception to be handled
+    Returns:
+        tuple: Redirects to the error page with the error description
+    """    
     kwargs = {
         "error_name": "400 Bad Request",
         "error_description": "Error en petición al servidor",
@@ -13,7 +17,11 @@ def bad_request_error(e):
 
 
 def unauthorized_error(e):
-    """HTTP Error 401 (Unauthorized)"""
+    """Args:
+        e (Exception): Exception to be handled
+    Returns:
+        tuple: Redirects to the error page with the error description
+    """ 
     kwargs = {
         "error_name": "401 Access Unauthorized",
         "error_description": "El accesso no está autorizado",
@@ -24,7 +32,11 @@ def unauthorized_error(e):
 
 
 def forbidden_error(e):
-    """HTTP Error 403 (Forbidden)"""
+    """Args:
+        e (Exception): Exception to be handled
+    Returns:
+        tuple: Redirects to the error page with the error description
+    """ 
     kwargs = {
         "error_name": "403 Access Forbidden",
         "error_description": "El acceso al recurso fue prohibido",
@@ -35,7 +47,11 @@ def forbidden_error(e):
 
 
 def not_found_error(e):
-    """HTTP Error 404 (Not Found)"""
+    """Args:
+        e (Exception): Exception to be handled
+    Returns:
+        tuple: Redirects to the error page with the error description
+    """ 
     kwargs = {
         "error_name": "404 Page Not Found",
         "error_description": "No se encontro la URL",
@@ -46,7 +62,11 @@ def not_found_error(e):
 
 
 def internal_server_error(e):
-    """HTTP Error 500 (Internal Server Error)"""
+    """Args:
+        e (Exception): Exception to be handled
+    Returns:
+        tuple: Redirects to the error page with the error description
+    """ 
     kwargs = {
         "error_name": "500 Internal Server Error",
         "error_description": "Error interno del servidor. Inténtelo de nuevo más tarde",
