fdd7088a46f7e0739a339d53650f670182b78afc
Ignacio Traberg <ignacio.traberg@gmail.com>
2022-10-17 16:22:06 -0300

    Payment Post API updated

--- admin/src/web/controllers/api/payment.py
+++ admin/src/web/controllers/api/payment.py
@@ -1,12 +1,8 @@
-from flask import Blueprint, redirect, url_for,flash
+from flask import Blueprint
 from src.web.helpers.build_response import response
-from src.core.board import create_associate, delete_associate, get_associate_by_id, list_associates, update_associate,add_discipline_to_associate,remove_discipline_to_associate,get_discipline,list_all_associates,list_all_disciplines
-from src.web.forms.associate import CreateAssociateForm, UpdateAssociateForm
-from src.web.helpers.writers import write_csv_file,write_pdf_file
-from src.web.helpers.auth import has_permission
-from src.web.helpers.pagination import pagination_generator
-from src.web.helpers.associate import no_es_moroso
-from src.web.helpers.pagination import pagination_generator
+from src.core.board import get_associate_by_id, get_last_fee_paid
+from src.web.helpers.payment_helpers import build_payment
+from src.core.board.repositories.payments import create_payment
 
 payment_api_blueprint = Blueprint(
     "payment_api", __name__, url_prefix="/payments"
@@ -33,42 +29,23 @@ def add_payment(id):
         id (int): Associate id
     Returns:
         Str: Success message
-    """    
-    create(id)
-    return response(200, "Pago agregado")
-
-
-def create(id):
-    """Args:
-        id (int): id of the associate to create a payment for
-    Returns:
-        HTML: Redirect to payments list.
-    """    
+    """
     associate=get_associate_by_id(id)
-    last_fee=get_last_fee_paid(associate)
-    config=get_cfg()
-    amount=disciplines_fee_amount(associate)+config.base_fee
-    paid_late=False
-    fee_date=datetime.now()
-    
-    if last_fee.installment_number!=0: #if the associate has paid at least one fee
-        
-        if last_fee.date.month==datetime.now().month and last_fee.date.year==datetime.now().year: #if the last fee was paid this month
-            flash(f"El asociado ya pago la cuota de este mes", category="alert alert-warning")
-            return redirect(url_for("associate.index"))
-        
-        elif (datetime.now()-last_fee.date).days>60: #if the last fee was paid more than one month ago
-            flash(f"El asociado ha pagado una cuota de un mes anterior", category="alert alert-warning")
-            amount+=amount*(config.due_fee/100)
-            fee_date=last_fee.date.replace(month=last_fee.date.month+1)
-            paid_late=True
-            
-        else:
-            flash(f"El asociado ha pagado la cuota exitosamente", category="alert alert-warning")
-            
+    last_fee=get_last_fee_paid(associate)  
+    flash_number,paid_late,fee_date,amount =build_payment(last_fee,associate)
+
+    associate=get_associate_by_id(id)
+    last_fee=get_last_fee_paid(associate)  
+    flash_number,paid_late,fee_date,amount =build_payment(last_fee,associate)
+    message = ""
+    if flash_number==1:
+        message = "Pago rechazado"
+        status = 400
     else:
-        flash(f"El asociado ha pagado la cuota exitosamente", category="alert alert-warning")
-            
+        message = "Pago registrado"
+        status = 200
+        
     create_payment(associate,amount,last_fee.installment_number,paid_late,fee_date)
-    return redirect(url_for("associate.index"))
+    return response(status, message)
+
 
