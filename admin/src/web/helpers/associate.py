64d4bd7e3f2956992ae5f84574b14923e38ec1e5
Lautaro Torchia <torchia.lautaro2@gmail.com>
2022-10-20 11:20:35 -0300

    fixed associate is_up_to_date function

--- admin/src/web/helpers/associate.py
+++ admin/src/web/helpers/associate.py
@@ -1,17 +1,20 @@
-
 from datetime import datetime
 from src.core.board.repositories.payments import get_last_fee_paid
 
 
-def no_es_moroso(associate):
+def is_up_to_date(associate):
     """Returns true if the associate is not in debt
     """    
     payment=get_last_fee_paid(associate)
+    
     if payment.installment_number==0:
-        if (datetime.now()-associate.entry_date).days<60:
+        if (datetime.now()-associate.entry_date).days<30:
             return True
         
-    elif (datetime.now()-payment.date).days<60:
+    elif (datetime.now().month==payment.date.month):
+        return True
+    
+    elif (datetime.now().month==payment.date.month+1 and datetime.now().day<=10):
         return True
     
     return False
