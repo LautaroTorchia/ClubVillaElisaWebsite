8cd781422583d85fefe2f9d03276f44d9df2a37d
fots <facundotomatis@gmail.com>
2022-10-20 06:05:29 -0300

    made receipt look better

--- admin/src/web/helpers/payment_helpers.py
+++ admin/src/web/helpers/payment_helpers.py
@@ -55,25 +55,29 @@ def build_payment(last_fee,associate):
 
 
 def make_receipt(payment,RCPT_PATH):
-    """Args:
+    """Builds a personalized PNG receipt file
+    Args:
         payment (Payment): Payment object
         RCPT_PATH (str): Path to save the receipt
     """    
     #draw a receipt for a payment
     dict_meses={1:"Enero",2:"Febrero",3:"Marzo",4:"Abril",5:"Mayo",6:"Junio",7:"Julio",8:"Agosto",9:"Septiembre",10:"Octubre",11:"Noviembre",12:"Diciembre"}
-    FONT_PATH=os.path.join(os.getcwd(),"public","font","Roboto-Bold.ttf")
-    receipt=Image.new("RGB",(1000,400),"white")
+    FONT_PATH=os.path.join(os.getcwd(),"public","fonts","Roboto-Bold.ttf")
+    FONT_TXT_PATH=os.path.join(os.getcwd(),"public","fonts","Roboto-Regular.ttf")
+    receipt=Image.new("RGBA",(850,300),"white")
     draw=ImageDraw.Draw(receipt)
     titleFont=ImageFont.truetype(FONT_PATH,size=25)
-    nameFont=ImageFont.truetype(FONT_PATH,size=20)
+    nameFont=ImageFont.truetype(FONT_TXT_PATH,size=20)
     
     logo=Image.open(os.path.join(os.getcwd(),"public","img","logo_club.png"))
-    receipt.paste(logo,(0,0))
+    logo=logo.convert("RGBA")
+    receipt.paste(logo,(15,40),logo)
     
-    draw.text((215,10),f"Recibo de pago #{payment.installment_number}",fill="black",font=titleFont)
-    draw.text((800,20),f"Fecha: {datetime.now().strftime('%d/%m/%Y')}",fill="black",font=nameFont)
-    draw.text((215,125),f"Recibimos de : {payment.associate.name} {payment.associate.surname}",fill="black",font=nameFont)
-    draw.text((215,175),f"El importe en pesos de ${payment.amount}",fill="black",font=nameFont)
-    draw.text((215,225),f"Por concepto de cuota societaria Para el mes de {dict_meses[payment.date.month]} {payment.date.year}",fill="black",font=nameFont)
+    from src.core.board.repositories.configuration import get_cfg
+    draw.text((230,10),f"Recibo de pago #{payment.installment_number}",fill="black",font=titleFont)
+    draw.text((615,20),f"Fecha: {datetime.now().strftime('%d/%m/%Y')}",fill="black",font=nameFont)
+    draw.text((230,125),f"Recibimos de : {payment.associate.name} {payment.associate.surname}",fill="black",font=nameFont)
+    draw.text((230,175),f"el importe ${payment.amount} {get_cfg().currency}",fill="black",font=nameFont)
+    draw.text((230,225),f"Por el concepto de cuota societaria mes {dict_meses[payment.date.month]} {payment.date.year}",fill="black",font=nameFont)
     receipt.save(RCPT_PATH)
 
