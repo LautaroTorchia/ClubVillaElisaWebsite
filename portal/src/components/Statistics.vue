4093e6c70257a0f522ebca0758b3dc14489bdee0
Ciuel <juantvicens@gmail.com>
2022-10-28 13:17:46 -0300

    Added statistics module

---
+++ portal/src/components/Statistics.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style = "width: 500px;">
+    <BarChart />
+  </div>
+</template>
+
+<script lang="ts">
+import BarChart from '../charts/chart'
+
+export default {
+  name: 'App',
+  components: { BarChart }
+}
+</script>
+
+<style>
+@media screen and (min-width: 300px) {
+  .chart {
+    width: 100%;
+  }
+}
+
+</style>
--- portal/src/routes.ts
+++ portal/src/routes.ts
@@ -2,9 +2,10 @@ import Home from './components/Home.vue'
 import Discipline from './components/Discipline.vue'
 import Payment from './components/Payment.vue'
 import Login from './components/Login.vue'
-import Estadisticas from './components/Estadisticas.vue'
+import Statistics from './components/Statistics.vue'
 
 const routes = [
+<<<<<<< Updated upstream
   { 
     name: "home",
     path: "/", 
@@ -31,5 +32,13 @@ const routes = [
     component: Login 
   },
 ];
+=======
+    { path: '/', component: Home },
+    { path: '/disciplinas', component: Discipline },
+    { path: '/pagos', component: Payment },
+    { path: '/estadisticas', component: Statistics },
+    { path: '/autenticar', component: Login },
+]
+>>>>>>> Stashed changes
 
 export default routes
