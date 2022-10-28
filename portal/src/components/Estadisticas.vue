36e1dabe21fbc8ceefecfe8de0aae26728c35847
Ignacio Traberg <ignacio.traberg@gmail.com>
2022-10-28 13:09:58 -0300

    Statistics basis

--- portal/src/components/Estadisticas.vue
+++ portal/src/components/Estadisticas.vue
@@ -5,10 +5,25 @@ export default {
 </script>
 
 <template>
-    <div>
+    <div class="card own_main_holder text-center">
         <h1>Estadisticas</h1>
+        <div class="card text-center statistics_body">
+
+        </div>
     </div>
+
 </template>
 
 <style scoped>
+.own_main_holder {
+    width: 100%;
+    padding: 20px;
+    align-items: center;
+    border: 0px;
+}
+.statistics_body {
+  margin-top: 20px;
+  width: 80%;
+  padding: 20px;
+}
 </style>
