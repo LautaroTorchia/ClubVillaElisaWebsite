c07e69075b0ed18e0fc844940263c54ff337a182
Ignacio Traberg <ignacio.traberg@gmail.com>
2022-11-02 20:58:38 -0300

    The table displays rows, but info is being fed to it wrong

--- portal/src/components/DisciplinesTable.vue
+++ portal/src/components/DisciplinesTable.vue
@@ -1,20 +1,22 @@
 
 <template>
     <div>
-        <p> {{ disciplines.keys }} </p>
+        <p> {{ disciplines }} </p>
         <table id="tableComponent" class="table table-bordered table-striped">
             <thead>
                 <tr>
-                <!-- loop through each value of the fields to get the table header -->
-                <th v-for="(k, v, index) in disciplines" :key='index' @click="sortTable(discipline)" > 
-                    {{k}} <i class="bi bi-sort-alpha-down" aria-label='Sort Icon'></i>
-                </th>
+                    <!-- loop through each value of the fields to get the table header -->
+                    <th v-for="(k, v, index) in disciplines" :key='index'> 
+                        {{k}} <i class="bi bi-sort-alpha-down" aria-label='Sort Icon'></i>
+                    </th>
                 </tr>
             </thead>
             <tbody>
                 <!-- Loop through the list get the each student data -->
-                <tr v-for="item in filteredList" :key='item'>
-                <td v-for="field in fields" :key='field'>{{item[field]}}</td>
+                <tr v-for="(k, v, index) in disciplines" :key='index'>
+                    <td v-for="(k, v, index2) in disciplines" :key='index2'>
+                        {{v}}
+                    </td>
                 </tr>
             </tbody>
         </table> 
@@ -31,6 +33,7 @@
         data() {
             return {
                 disciplines: [] as DisciplineClub[]
+
             }
         },
         components: {
