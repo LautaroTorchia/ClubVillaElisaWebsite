1cc5409886b59f914815f1b8f6889e4dbd895dc6
fots <facundotomatis@gmail.com>
2022-11-14 16:51:14 -0300

    modified axios config to support credentials

--- portal/src/services/axios.ts
+++ portal/src/services/axios.ts
@@ -1,6 +1,8 @@
 import axios, { AxiosInstance } from 'axios'
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: 'http://localhost:5000/api', // TODO: make it change depending on environment
+  withCredentials: true,
+  xsrfCookieName: 'csrf_access_token',
   headers: {
     'Content-Type': 'application/json',
   },
