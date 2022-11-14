190dfebba7c8f69fa7e0f7337e93d0f03303e8c6
fots <facundotomatis@gmail.com>
2022-11-14 16:56:01 -0300

    used disciplines to show user session

--- portal/src/components/Discipline.vue
+++ portal/src/components/Discipline.vue
@@ -1,15 +1,20 @@
-
 <script lang="ts">
-  export default {
-    setup() {},
-  }
+import { defineComponent } from 'vue'
+import { mapGetters } from 'vuex'
+export default defineComponent({
+  computed: {
+    ...mapGetters({
+      authUser: 'auth/user',
+    }),
+  },
+})
 </script>
 
 <template>
   <div>
     <h1>Disciplinas</h1>
+    <p>Usuario: {{ authUser.username }}</p>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
