c49995db8866c2fd67dc0f50963cfb5f7132d405
fots <facundotomatis@gmail.com>
2022-11-16 00:04:25 -0300

    changed parameter type of id

--- portal/src/services/PaymentService.ts
+++ portal/src/services/PaymentService.ts
@@ -1,13 +1,13 @@
 import axios from './axios'
 import { PaymentResponse } from '../interfaces/responses/PaymentResponse'
 
-export const getPayments = async (id: string): Promise<PaymentResponse> => {
+export const getPayments = async (id: number): Promise<PaymentResponse> => {
   const response = await axios.get('/me/payments/' + id)
   return response.data
 }
 
 
-export const createPayment = async (id:string): Promise<PaymentResponse> => {
+export const createPayment = async (id:number): Promise<PaymentResponse> => {
   const response = await axios.post('/me/payments/' + id)
   return response.data
 }
