648d9c1dfce3d177051e83243231fc3c0ec951c0
Lautaro Torchia <torchia.lautaro2@gmail.com>
2022-11-17 11:41:22 -0300

    first step in fixing problem

--- portal/src/components/Discipline.vue
+++ portal/src/components/Discipline.vue
@@ -1,44 +1,44 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { Discipline } from '../interfaces/Discipline'
-import { Card } from '../interfaces/Card'
-
-import { getMyDisciplines } from '../services/DisciplinesService'
-import { getMyCard } from "../services/AssociateDataService";
-export default defineComponent({
-  data() {
-    return {
-      disciplines: [] as Discipline[] | string,
-      associate_card: [] as Card[] | string,
+  import { defineComponent } from 'vue'
+  import { Discipline } from '../interfaces/Discipline'
+  import { Card } from '../interfaces/Card'
+  import { getMyDisciplines } from '../services/DisciplinesService'
+  import { getMyCard } from "../services/AssociateDataService";
+  import { mapGetters } from 'vuex'
+  export default defineComponent({
+    data() {
+      return {
+        disciplines: [] as Discipline[] | string,
+        associate_card: [] as Card[] | string,
+      }
+    },
+    computed: {
+      ...mapGetters({
+        authUser: 'auth/user',
+      }),
+    },
+    methods: {
+      async loadMyDisciplines() {
+        const id = '2' // WARNING: get from jwt
+        const res = await getMyDisciplines(id)
+        this.disciplines = res.data
+      },
+      async loadMyCard() {
+        const id = '1' // WARNING: get from jwt
+        const res = await getMyCard(id)
+        this.associate_card= res.data
+      },
+    },
+    mounted() {
+      this.loadMyDisciplines()
+      this.loadMyCard()
     }
-  },
-  computed: {
-    ...mapGetters({
-      authUser: 'auth/user',
-    }),
-  },
-  methods: {
-    async loadMyDisciplines() {
-      const id = '2' // WARNING: get from jwt
-      const res = await getMyDisciplines(id)
-      this.disciplines = res.data
-    },
-    async loadMyCard() {
-      const id = '1' // WARNING: get from jwt
-      const res = await getMyCard(id)
-      this.associate_card = res.data
-    },
-  },
-  mounted() {
-    this.loadMyDisciplines()
-    this.loadMyCard()
-
+})
 </script>
-
 <template>
   <div>
-    <h1>Mis Disciplinas</h1>
-    <p>Usuario: {{ authUser.username }}</p>
+    <h1>Datos de {{ authUser.username }}</h1>
+    <h2>Disciplinas:</h2>
     <div v-for="(discipline, index) in disciplines" :key="index">
       <p>Disciplina: {{ discipline.name }} | Categoria: {{ discipline.category }}</p>
       <p>Precio: {{ discipline.price }} | Instructores: {{ discipline.teacher }}</p>
@@ -46,10 +46,8 @@ export default defineComponent({
     </div>
   </div>
   <div>
-    <h1>Mi Carnet</h1>
-     {{ associate_card.associate_card }}
-    <h1>Disciplinas</h1>
-    
+    <h2>Mi Carnet</h2>
+    <img :src="'data:image/jpg;base64,' + associate_card.associate_card " alt="Associate Carnet" />
   </div>
 </template>
 
