890284ce0d8b794240e5966aa1bbe6183718c613
Lautaro Torchia <torchia.lautaro2@gmail.com>
2022-11-17 13:50:18 -0300

    quick fix to payments

--- portal/src/services/PaymentService.ts
+++ portal/src/services/PaymentService.ts
@@ -1,13 +1,13 @@
 import axios from './axios'
 import { PaymentResponse } from '../interfaces/responses/PaymentResponse'
 
-export const getPayments = async (): Promise<PaymentResponse> => {
-  const response = await axios.get('/me/payments/')
+export const getPayments = async (id:number): Promise<PaymentResponse> => {
+  const response = await axios.get('/me/payments/' + id)
   return response.data
 }
 
 
-export const createPayment = async (): Promise<PaymentResponse> => {
-  const response = await axios.post('/me/payments/')
+export const createPayment = async (id:number): Promise<PaymentResponse> => {
+  const response = await axios.post('/me/payments/' + id)
   return response.data
 }
