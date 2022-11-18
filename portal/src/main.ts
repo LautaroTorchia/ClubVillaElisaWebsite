fba38bb02e86dd2aa6702784621426aab35d1bbb
fots <facundotomatis@gmail.com>
2022-11-18 04:31:15 -0300

    added modals as plugin and moved router definition into own file

--- portal/src/main.ts
+++ portal/src/main.ts
@@ -1,22 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
-import routes from './routes'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@coreui/coreui/dist/css/coreui.min.css'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import './icons.ts'
 import store from './stores'
+import { vfmPlugin } from 'vue-final-modal'
+import router from './router'
 
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-  linkActiveClass: 'active',
-})
 createApp(App)
   .use(router)
   .use(store)
+  .use(vfmPlugin)
   .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app')
