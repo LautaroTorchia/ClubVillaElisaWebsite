773b3bd09579a21fa0de1dc327d5d03d8347ba72
fots <facundotomatis@gmail.com>
2022-11-21 15:55:40 -0300

    fixed user modification

--- admin/src/web/forms/user.py
+++ admin/src/web/forms/user.py
@@ -73,32 +73,11 @@ class BasicUserForm(BaseForm):
             Boolean: True if the DNI number is not in use
         """
         if self.user_id:
-            if get_user_by(self.user_id).username != field.data and get_user_by(
-                field.data, "username"
-            ):
-                raise ValidationError("Nombre de usuario ya registrado")
-        else:
+            if get_user_by(self.user_id).username != field.data:
+                raise ValidationError("No se puede cambiar el nombre de usuario")
+        else: 
             if get_user_by(field.data, "username"):
                 raise ValidationError("Nombre de usuario ya registrado")
-        return True
-
-    def validate_roles(self, field):
-        """Args:
-            form (CreateAssociateForm): Form to create a new associate
-            field (DNI_number): DNI number of the associate
-        Raises:
-            ValidationError: If the DNI number is already in use
-        Returns:
-            Boolean: True if the DNI number is not in use
-        """
-
-        if self.user_id:
-            r = get_user_by(self.user_id).roles
-            print(r)
-            is_associate = list(filter(lambda x: x.name == "Socio", r)) != []
-            if is_associate:
-                raise ValidationError("No se puede cambiar el rol socio")
-
         return True
 
 
@@ -111,7 +90,6 @@ class UserForm(BasicUserForm):
     password: Password of the user
     roles: Roles of the user
     """
-
     password = PasswordField(
         "Contraseña", validators=[Length(max=255), InputRequired()]
     )
